06d6d92fc448481675d0bb5c2651d89c94a11b29
Dias <andremssdias@hotmail.com>
2025-03-20 22:02:07 +0100

    Feat(blogs): List all posts.

--- apps/blogs/views/index.py
+++ apps/blogs/views/index.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from apps.blogs.forms.post_form import PostForm
+from apps.blogs.models import Post
 
 User = get_user_model()
 
 
-class TenantDashboardView(TemplateView):
+class TenantDashboardView(ListView):
+    model = Post
     template_name = "blogs/index.html"
+    context_object_name = "posts"
 
     def get_context_data(self, **kwargs):
         """Check for auth token and provide user data in template context."""
