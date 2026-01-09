f3a88d8d835e37e069d93a30a6e534538a53c611
Dias <andremssdias@hotmail.com>
2026-01-09 22:37:55 +0100

    refactor(users): Update logout post functionality.

--- apps/users/views/logout.py
+++ apps/users/views/logout.py
@@ -5,23 +5,6 @@ from django.core.cache import cache
 
 class LogoutCustomView(LogoutView):
 
-    def post(self, request, *args, **kwargs):
-        request = self.request
-
-        auth_token = request.COOKIES.get("auth_token")
-
-        if auth_token:
-            cache.delete(f"auth_token:{auth_token}")
-
-        response = super().post(request, *args, **kwargs)
-
-        current_host = request.get_host().split(":")[0]
-
-        # Clear session cookie
-        response.delete_cookie(settings.COOKIE_NAME, domain=f".{current_host}")
-
-        return response
-
     def get_success_url(self):
         if hasattr(self.request, "tenant"):
             schema = "http" if settings.DEBUG else "https"
