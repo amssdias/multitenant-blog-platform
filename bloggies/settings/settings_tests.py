02a7eaf488c47367e19e975e5af306d692fc9db1
Dias <andremssdias@hotmail.com>
2026-01-20 19:25:59 +0100

    fix(config): change session engine on tests config.

--- bloggies/settings/settings_tests.py
+++ bloggies/settings/settings_tests.py
@@ -9,3 +9,5 @@ CELERY_TASK_EAGER_PROPAGATES = True
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 DEFAULT_FROM_EMAIL = "noreply@bloggies.test"
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
